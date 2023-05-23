0d2dd0afc299ab3cf300a9b2a123e4d1e2ed47d7
karanraiyanikkr3 <127606842+karanraiyanikkr3@users.noreply.github.com>
2023-05-23 15:27:36 +0530

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -15,14 +15,14 @@ def call(String URL) {
 //        echo '${config.url}'
 
         // Building Docker image
-        sh "docker build -t ${env.IMAGE_REPO_NAME} ."
-        sh "docker images"
+        sh "sudo docker build -t ${env.IMAGE_REPO_NAME} ."
+        sh "sudo docker images"
 
         // Pushing to ECR
         withCredentials([string(credentialsId: 'AWS_ACCOUNT_ID', variable: 'AWS_ACCOUNT_ID')]) {
             def REPOSITORY_URI = "${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/jenkinsbuild"
-            sh "docker tag ${env.IMAGE_REPO_NAME}:${env.IMAGE_TAG} ${REPOSITORY_URI}:${env.IMAGE_TAG}"
-            sh "docker push ${REPOSITORY_URI}:${env.IMAGE_TAG}"
+            sh "sudo docker tag ${env.IMAGE_REPO_NAME}:${env.IMAGE_TAG} ${REPOSITORY_URI}:${env.IMAGE_TAG}"
+            sh "sudo docker push ${REPOSITORY_URI}:${env.IMAGE_TAG}"
         }
     } catch (Exception e) {
         echo "Failed to build and push Docker image: ${e.getMessage()}"
