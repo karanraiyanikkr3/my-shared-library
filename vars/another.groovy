2e6caab338a8eee641c51c5c2b0fe42eccd933fd
karanraiyanikkr3 <127606842+karanraiyanikkr3@users.noreply.github.com>
2023-06-01 21:54:10 +0530

    Update another.groovy

--- vars/another.groovy
+++ vars/another.groovy
@@ -16,7 +16,7 @@ def anotherMethod(String DIR,String PORTAL) {
 //                    sh "sudo docker build -t ${REPOSITORY_URI}:${env.IMAGE_TAG} $DIR"
         sh "echo ${env.REPOSITORY_URI}"
         //    docker tag masterportal:latest 885753452070.dkr.ecr.us-east-1.amazonaws.com/masterportal:latest
-              sh "sudo docker tag ${PORTAL}:${env.IMAGE_TAG}  :${env.IMAGE_TAG}"
+           //   sh "sudo docker tag ${PORTAL}:${env.IMAGE_TAG}  :${env.IMAGE_TAG}"
               sh "sudo docker images"
         
 
