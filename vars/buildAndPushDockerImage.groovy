324d16e792c6ac7badcfb7d4892613e73bf4ca82
karanraiyanikkr3 <127606842+karanraiyanikkr3@users.noreply.github.com>
2023-06-01 12:04:44 +0530

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -2,31 +2,22 @@ def call(String DIR) {
     env.AWS_DEFAULT_REGION = "us-east-1"
     env.IMAGE_REPO_NAME = "masterportal"
     env.IMAGE_TAG = "latest"
+    env.AWS_ACCOUNT_ID = "885753452070"
+    def REPOSITORY_URI = "${AWS_ACCOUNT_ID}.dkr.ecr.${env.AWS_DEFAULT_REGION}.amazonaws.com/${env.IMAGE_REPO_NAME}"
 
     try {
         // Logging into AWS ECR
-        withCredentials([string(credentialsId: 'AWS_ACCOUNT_ID', variable: 'AWS_ACCOUNT_ID')]) {
             sh "aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
-        }
-
-        // Cloning Git
-        //git 'https://github.com/karanraiyanikkr3/node-todo-cicd.git/'
-       // git url:URL
-//        echo '${config.url}'
-
+        
         // Building Docker image
-       
+            sh "sudo docker build -t ${REPOSITORY_URI}:${env.IMAGE_TAG} $DIR"
+            sh "sudo docker images"
         
 
         // Pushing to ECR
-        withCredentials([string(credentialsId: 'AWS_ACCOUNT_ID', variable: 'AWS_ACCOUNT_ID')]) {
-            //def REPOSITORY_URI = "${AWS_ACCOUNT_ID}.dkr.ecr.${env.AWS_DEFAULT_REGION}.amazonaws.com/${env.IMAGE_REPO_NAME}"
-           // sh "sudo docker tag ${env.IMAGE_REPO_NAME}:${env.IMAGE_TAG} ${REPOSITORY_URI}:${env.IMAGE_TAG}"
-            sh "sudo docker build -t ${REPOSITORY_URI}:${env.IMAGE_TAG} $DIR"
-            sh "sudo docker images"
             sh "sudo docker push ${REPOSITORY_URI}:${env.IMAGE_TAG}"
         }
-    } catch (Exception e) {
+     catch (Exception e) {
         echo "Failed to build and push Docker image: ${e.getMessage()}"
         error "Building and pushing Docker image failed"
     }
